d88a020f434b6b75e798a6e2b9e5101539d2d237
Batman-45 <rohitdeshmane7@gmail.com>
2026-07-28 12:42:37 +0530

    Fix Ritual logo import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import {
   Search,
 } from "lucide-react";
 
-import logo from "../assets/ritual-logo.jpg";
+import logo from "../assets/ritual-logo.png";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
